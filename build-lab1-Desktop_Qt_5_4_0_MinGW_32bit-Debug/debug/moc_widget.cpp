9d46afe28e7d6a551a17c769fc48b6f418045b2c
nastya_dubovik <stasyadybovik@gmail.com>
2015-03-19 01:30:14 +0300

    [+] debug

---
+++ build-lab1-Desktop_Qt_5_4_0_MinGW_32bit-Debug/debug/moc_widget.cpp
@@ -0,0 +1,144 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../lab1/widget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_Widget_t {
+    QByteArrayData data[11];
+    char stringdata[106];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Widget_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Widget_t qt_meta_stringdata_Widget = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Widget"
+QT_MOC_LITERAL(1, 7, 5), // "score"
+QT_MOC_LITERAL(2, 13, 0), // ""
+QT_MOC_LITERAL(3, 14, 9), // "score_two"
+QT_MOC_LITERAL(4, 24, 10), // "timer_slot"
+QT_MOC_LITERAL(5, 35, 14), // "timer_slot_two"
+QT_MOC_LITERAL(6, 50, 5), // "start"
+QT_MOC_LITERAL(7, 56, 12), // "fast_checked"
+QT_MOC_LITERAL(8, 69, 12), // "slow_checked"
+QT_MOC_LITERAL(9, 82, 11), // "one_checked"
+QT_MOC_LITERAL(10, 94, 11) // "two_checked"
+
+    },
+    "Widget\0score\0\0score_two\0timer_slot\0"
+    "timer_slot_two\0start\0fast_checked\0"
+    "slow_checked\0one_checked\0two_checked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Widget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       9,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   59,    2, 0x0a /* Public */,
+       3,    0,   60,    2, 0x0a /* Public */,
+       4,    0,   61,    2, 0x0a /* Public */,
+       5,    0,   62,    2, 0x0a /* Public */,
+       6,    0,   63,    2, 0x0a /* Public */,
+       7,    0,   64,    2, 0x0a /* Public */,
+       8,    0,   65,    2, 0x0a /* Public */,
+       9,    0,   66,    2, 0x0a /* Public */,
+      10,    0,   67,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Widget *_t = static_cast<Widget *>(_o);
+        switch (_id) {
+        case 0: _t->score(); break;
+        case 1: _t->score_two(); break;
+        case 2: _t->timer_slot(); break;
+        case 3: _t->timer_slot_two(); break;
+        case 4: _t->start(); break;
+        case 5: _t->fast_checked(); break;
+        case 6: _t->slow_checked(); break;
+        case 7: _t->one_checked(); break;
+        case 8: _t->two_checked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject Widget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_Widget.data,
+      qt_meta_data_Widget,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *Widget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Widget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_Widget.stringdata))
+        return static_cast<void*>(const_cast< Widget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 9;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
